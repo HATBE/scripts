96a02461245736a19635826a7da017046712162f
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-12-30 20:11:59 +0100

    fix index override in array (orgs / user)

--- php/getGithubReposFromUser.php
+++ php/getGithubReposFromUser.php
@@ -9,6 +9,8 @@
     $orgs = [];
     $output = [];
 
+    $key = 0;
+
     function apiGet($url, $path) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url . $path);
@@ -18,44 +20,49 @@
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36');
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
-            exit();
+            return 'Error: ' . curl_error($ch);
         } else {
             return json_decode($result, true);
         }
         curl_close($ch);
     }
 
-    function appendRepoToOutput(&$output, $key, $repo) {
-        global $api;
-        $output['repos'][$key]['name'] = htmlentities($repo['name'], ENT_QUOTES);
-        $output['repos'][$key]['fullName'] = htmlentities($repo['full_name'], ENT_QUOTES);
-        $output['repos'][$key]['description'] = !empty($repo['description']) ? htmlentities($repo['description'], ENT_QUOTES) : null;
-        $output['repos'][$key]['created'] = htmlentities(strtotime($repo['created_at']), ENT_QUOTES);
-        $output['repos'][$key]['pushed'] = htmlentities(strtotime($repo['pushed_at']), ENT_QUOTES);
-        $output['repos'][$key]['stars'] = htmlentities($repo['stargazers_count'], ENT_QUOTES);
+    function appendRepoToOutput($api, &$output, $repo, &$key) {
+        $key++;
+        $output[$key]['name'] = htmlentities($repo['name'], ENT_QUOTES);
+        $output[$key]['fullName'] = htmlentities($repo['full_name'], ENT_QUOTES);
+        $output[$key]['description'] = !empty($repo['description']) ? htmlentities($repo['description'], ENT_QUOTES) : 'null';
+        $output[$key]['created'] = htmlentities(strtotime($repo['created_at']), ENT_QUOTES);
+        $output[$key]['pushed'] = htmlentities(strtotime($repo['pushed_at']), ENT_QUOTES);
+        $output[$key]['stars'] = htmlentities($repo['stargazers_count'], ENT_QUOTES);
         foreach(apiGet($api, "repos/{$repo['full_name']}/languages") as $lang=>$c) {
-            $output['repos'][$key]['languages'][] = htmlentities($lang, ENT_QUOTES);
+            $output[$key]['languages'][] = htmlentities($lang, ENT_QUOTES);
         }
+        $output[$key]['languages'] = isset($output[$key]['languages']) ? $output[$key]['languages'] : 'null';
     }
 
     foreach(apiGet($api, "users/{$user}/orgs") as $org) {
         array_push($orgs, $org['login']);
     }
 
-    foreach(apiGet($api, "users/{$user}/repos") as $key=>$repo) {
-        appendRepoToOutput($output, $key, $repo);
+    foreach(apiGet($api, "users/{$user}/repos") as $repo) {
+        appendRepoToOutput($api, $output, $repo, $key);
     }
 
     foreach($orgs as $org) {
-        foreach(apiGet($api, "orgs/{$org}/repos") as $key=>$repo) {
-            appendRepoToOutput($output, $key, $repo);
+        foreach(apiGet($api, "orgs/{$org}/repos") as $repo) {
+            appendRepoToOutput($api, $output, $repo, $key);
         }
     }
 
-    $output['time'] = time();
+    print_r($output);
 
-    $json = json_encode($output);
+    array_multisort(array_column($output, 'pushed'), SORT_DESC, $output);
+
+    $finalOutput['repos'] = $output;
+    $finalOutput['time'] = time();
+
+    $json = json_encode($finalOutput);
 
     $file = fopen(__DIR__ . '/repos.json', 'w');
     fwrite($file, $json);
