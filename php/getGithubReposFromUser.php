5ad621cf50f189e2b979eaa7dcae27dc6faf15b9
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2022-01-11 16:50:09 +0100

    Change api

--- php/getGithubReposFromUser.php
+++ php/getGithubReposFromUser.php
@@ -1,21 +1,24 @@
 <?php
+    require_once(__DIR__ . '/../../config/auth.php');
+
     $user = '<user>';
     define('GITHUB_USER', '<user>');
     define('GITHUB_TOKEN', '<token>');
 
-    $api = 'https://api.github.com/';
-    $orgs = [];
     $output = [];
-
     $key = 0;
 
-    function apiGet($url, $path) {
+    function callApi($query, $variables) {
+        $json = json_encode(['query' => $query, 'variables' => $variables]);
+        
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url . $path);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/graphql');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_USERPWD, GITHUB_USER . ':' . GITHUB_TOKEN);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'bitsflipped.ch - badges - BOT');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
             return 'Error: ' . curl_error($ch);
@@ -23,37 +26,97 @@
             return json_decode($result, true);
         }
         curl_close($ch);
+
+        return $result;
     }
 
-    function appendRepoToOutput($api, &$output, $repo, &$key) {
+    function getData($username) {
+        $query = '
+        query userData($login: String!) {
+            user(login: $login) {
+                repositories (first: 100, privacy: PUBLIC, orderBy: {direction: DESC, field: STARGAZERS}) {
+                    nodes {
+                        name
+                        nameWithOwner
+                        stargazerCount
+                        createdAt
+                        pushedAt
+                        description
+                        url
+                        owner {
+                            login
+                        }
+                        languages(first: 100) {
+                            nodes {
+                                name
+                                color
+                            }
+                        }
+                    }
+                }
+                repositoriesContributedTo (first: 100, privacy: PUBLIC, orderBy: {direction: DESC, field: STARGAZERS}) {
+                    nodes {
+                        name
+                        nameWithOwner
+                        stargazerCount
+                        createdAt
+                        pushedAt
+                        description
+                        url
+                        owner {
+                            login
+                        }
+                        languages(first: 100) {
+                            nodes {
+                                name
+                                color
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        ';
+
+        $variables = json_encode(['login' => $username]);
+        $data = callApi($query, $variables);
+
+        return $data;
+    }
+
+    function appendRepoToOutput(&$output, $repo, &$key) {
+        $output[$key]['owner'] = htmlspecialchars($repo['owner']['login']);
+        $output[$key]['name'] = htmlspecialchars($repo['name']);
+        $output[$key]['fullName'] = htmlspecialchars($repo['nameWithOwner']);
+        $output[$key]['description'] = !empty($repo['description']) ? htmlspecialchars($repo['description']) : null;
+        $output[$key]['created'] = htmlspecialchars(strtotime($repo['createdAt']));
+        $output[$key]['pushed'] = htmlspecialchars(strtotime($repo['pushedAt']));
+        $output[$key]['stars'] = htmlspecialchars($repo['stargazerCount']);
+        $subkey = 0;
+        foreach($repo['languages']['nodes'] as $lang) {
+            $output[$key]['languages'][$subkey]['language'] = $lang ['name'];
+            $output[$key]['languages'][$subkey]['color'] = $lang ['color'];
+            $subkey++;
+        }
         $key++;
-        $output[$key]['name'] = htmlentities($repo['name'], ENT_QUOTES);
-        $output[$key]['fullName'] = htmlentities($repo['full_name'], ENT_QUOTES);
-        $output[$key]['description'] = !empty($repo['description']) ? htmlentities($repo['description'], ENT_QUOTES) : 'null';
-        $output[$key]['created'] = htmlentities(strtotime($repo['created_at']), ENT_QUOTES);
-        $output[$key]['pushed'] = htmlentities(strtotime($repo['pushed_at']), ENT_QUOTES);
-        $output[$key]['stars'] = htmlentities($repo['stargazers_count'], ENT_QUOTES);
-        foreach(apiGet($api, "repos/{$repo['full_name']}/languages") as $lang=>$c) {
-            $output[$key]['languages'][] = htmlentities($lang, ENT_QUOTES);
+    }
+
+    function addAllRepos($data, &$output, &$key) {
+        $data = $data['data']['user'];
+        $own = $data['repositories']['nodes'];
+        $rct = $data['repositoriesContributedTo']['nodes'];
+
+        foreach($own as $repo) {
+            appendRepoToOutput($output, $repo, $key);
         }
-        $output[$key]['languages'] = isset($output[$key]['languages']) ? $output[$key]['languages'] : 'null';
-    }
 
-    foreach(apiGet($api, "users/{$user}/orgs") as $org) {
-        array_push($orgs, $org['login']);
-    }
-
-    foreach(apiGet($api, "users/{$user}/repos") as $repo) {
-        appendRepoToOutput($api, $output, $repo, $key);
-    }
-
-    foreach($orgs as $org) {
-        foreach(apiGet($api, "orgs/{$org}/repos") as $repo) {
-            appendRepoToOutput($api, $output, $repo, $key);
+        foreach($rct as $repo) {
+            appendRepoToOutput($output, $repo, $key);
         }
     }
 
-    print_r($output);
+    $data = getData($user);
+    addAllRepos($data, $output, $key);
 
     array_multisort(array_column($output, 'pushed'), SORT_DESC, $output);
 
@@ -62,5 +125,5 @@
 
     $json = json_encode($finalOutput);
 
-    $file = fopen(__DIR__ . '/repos.json', 'w');
+    $file = fopen(__DIR__ . '/../data/repos.json', 'w');
     fwrite($file, $json);
